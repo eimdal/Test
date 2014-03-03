895c342aab5d351a6069d43bec68b58863e1bca2
eimdal <alguillenar@gmail.com>
2014-03-03 15:22:40 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+/**
+  * Test.java
+  * Class for test github
+  * @author eimdal
+  */
+
+public class Test{
+
+    public static void main(String[]args){
+    
+        System.out.println("Hello World");
+        
+    }
+    
+}
